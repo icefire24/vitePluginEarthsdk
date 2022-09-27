fedb3153b6eae0872f520053d9a1d9b253df0bc2
baishun <863548227@qq.com>
2022-09-27 21:08:15 +0800

    feat:初始化

---
+++ index.d.ts
@@ -0,0 +1,3 @@
+import { Plugin } from "vite";
+declare function vitePluginEarthsdk(): Plugin;
+export { vitePluginEarthsdk as default };
---
+++ index.js
@@ -0,0 +1,46 @@
+import fs from "fs-extra";
+export default function vitePluginEarthsdk(){
+  let outDir = "dist";
+  let base = "/";
+  return {
+    name: "vitePluginEarthsdk",
+    async config(config, { command }) {
+      if (config.base) {
+        base = config.base;
+        if (base === "") base = "./";
+      }
+      if (command == "serve") {
+        //develop引入earth包
+        try {
+          await fs.copy("node_modules/earthsdk/dist/XbsjCesium", "public/XbsjCesium");
+          await fs.copy("node_modules/earthsdk/dist/XbsjEarth", "public/XbsjEarth");
+        } catch (err) {
+          console.error("copy failed", err);
+        }
+      } else {
+        outDir = config.build.outDir;
+      }
+    },
+    async closeBundle() {
+      //product引入earth包
+      try {
+        await fs.copy("node_modules/earthsdk/dist/XbsjCesium", outDir + "/XbsjCesium");
+        await fs.copy("node_modules/earthsdk/dist/XbsjEarth", outDir + "/XbsjEarth");
+      } catch (err) {
+        console.error("copy failed", err);
+      }
+    },
+    transformIndexHtml() {
+      //index.html script引入
+      return [
+        {
+          tag: "script",
+          attrs: {
+            src: "XbsjEarth/XbsjEarth.js",
+          },
+          injectTo: "head-prepend",
+        },
+      ];
+    },
+  };
+}
